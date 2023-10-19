6b309b8b2c19b54cda82beeb8e505c41ada47300
Mike McGowan <mike.mcgowan@brilliant.org>
2023-10-18 22:56:20 -0700

    make it way more fun

--- src/BouncingDiv.jsx
+++ src/BouncingDiv.jsx
@@ -7,21 +7,36 @@ const Box = styled.div`
     cursor: pointer;
 `;
 
+const Sparkle = styled.div`
+    position: absolute;
+    font-size: 64px;
+`;
+
 const BouncingDiv = () => {
     const boxRef = useRef();
     const [yipOrSkip, setYipOrSkip] = useState('yippee')
+    const [showSparkles, setShowSparkles] = useState(false)
+    const [sparkleCoords, setSparkleCoords] = useState([0, 0])
 
-    const toggle = () => {
+    const toggle = event => {
         const newYipOrSkip = yipOrSkip === 'yippee' ? 'skippee' : 'yippee'
+        console.log({event})
+        const x = event.pageX
+        const y = event.pageY
         setYipOrSkip(newYipOrSkip)
+        setSparkleCoords([x, y])
+        setShowSparkles(true)
+        setTimeout(() => {
+            setShowSparkles(false)
+        }, 500)
     }
 
     useEffect(() => {
         const box = boxRef.current;
         let x = Math.random() * (window.innerWidth - box.offsetWidth);
         let y = Math.random() * (window.innerHeight - box.offsetHeight);
-        let dx = (Math.random() - 0.5) * 12;
-        let dy = (Math.random() - 0.5) * 12;
+        let dx = (Math.random() - 0.5) * 7;
+        let dy = (Math.random() - 0.5) * 7;
         const animate = () => {
             if (x < 0 || x > window.innerWidth - box.offsetWidth) dx = -dx;
             if (y < 0 || y > window.innerHeight - box.offsetHeight) dy = -dy;
@@ -35,7 +50,13 @@ const BouncingDiv = () => {
         animate();
     }, []);
 
-    return <Box ref={boxRef} onClick={toggle}>{yipOrSkip}</Box>;
+
+    return (
+        <>
+            <Box ref={boxRef} onClick={toggle} className={`${showSparkles ? 'sparkle' : ''}`}>{yipOrSkip}</Box>
+            {showSparkles && <Sparkle className="sparkle" style={{ left: sparkleCoords[0], top: sparkleCoords[1] }}>✨</Sparkle>}
+        </>
+    )
 }
 
 export default BouncingDiv
